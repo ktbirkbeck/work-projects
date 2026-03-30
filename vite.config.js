4af565a07307dfdaf3636b95ded2be2605ddf574
ktbirkbeck <ktbirkbeck@gmail.com>
2026-03-30 09:47:18 -0400

    Add esbuild loader configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
